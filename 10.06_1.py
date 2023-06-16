b2d8f99e4a92769035f7b182c8b32b6647e31be8
Vahag <135336961+Vahag5@users.noreply.github.com>
2023-06-16 10:11:38 +0400

    Add files via upload

---
+++ 10.06_1.py
@@ -0,0 +1,6 @@
+def starts_with(str1, str2) :
+    return str1.startswith(str2)    #ogtagorcenq startswith metod@
+
+
+print (starts_with('parahandes', 'par')) # True a tpelu
+print (starts_with('urax', 'txur'))      # False a tpelu 
