a8ea3c93f8df3ccee5b1e7861347ccb896703f83
Vahag <135336961+Vahag5@users.noreply.github.com>
2023-06-16 10:19:54 +0400

    Add files via upload

---
+++ 10.06_2.py
@@ -0,0 +1,5 @@
+def end_with(str1, str2):
+	return str1.endswith(str2)  #ogtagorcenq endswith metod@
+
+print(end_with('vip', 'ip')) 		# True a tpelu
+print(end_with('arag', 'dandax')) 	# False a tpelu 
