f9b9264d06200ab3b972cd027966806577b1c2c1
Vahag <135336961+Vahag5@users.noreply.github.com>
2023-06-18 12:47:23 +0400

    Add files via upload

---
+++ 10.06_6.py
@@ -0,0 +1,13 @@
+def rotate_by(arr, qanak):
+    if qanak < 0:
+        qanak = len(arr) + qanak
+
+    qanak = qanak % len(arr)           # vor ete qanak@ mec exav massivi erkarytyunic tarberutyan chapov frcni
+    farr = arr[qanak:] + arr[:qanak]   #farr = frcrac arr
+    return farr
+
+
+
+arr = [1, 2, 3, 4, 5, 6, 7, 8]
+qanak= 10
+print(rotate_by(arr, qanak))  
