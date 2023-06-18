38ed43c939590310d02116007b34780048f254b2
Vahag <135336961+Vahag5@users.noreply.github.com>
2023-06-18 12:48:35 +0400

    Update 10.06_6.py

--- 10.06_6.py
+++ 10.06_6.py
@@ -1,12 +1,7 @@
 def rotate_by(arr, qanak):
-    if qanak < 0:
-        qanak = len(arr) + qanak
-
     qanak = qanak % len(arr)           # vor ete qanak@ mec exav massivi erkarytyunic tarberutyan chapov frcni
     farr = arr[qanak:] + arr[:qanak]   #farr = frcrac arr
     return farr
-
-
 
 arr = [1, 2, 3, 4, 5, 6, 7, 8]
 qanak= 10
