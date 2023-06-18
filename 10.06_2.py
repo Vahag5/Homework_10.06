ea80e27fbcc5b9ad3827d74d56f7a05b73354869
Vahag <135336961+Vahag5@users.noreply.github.com>
2023-06-18 11:19:09 +0400

    Update 10.06_2.py

--- 10.06_2.py
+++ 10.06_2.py
@@ -1,5 +1,6 @@
 def end_with(str1, str2):
-	return str1.endswith(str2)  #ogtagorcenq endswith metod@
+    s = len(str2)
+    return str1[-s:] == str2   
 
-print(end_with('vip', 'ip')) 		# True a tpelu
-print(end_with('arag', 'dandax')) 	# False a tpelu 
+print(end_with('vip', 'ip')) # True a tpelu
+print(end_with('urax', 'pix')) # False a tpelu 
