c088b062d3302e27326238fbf047bacb8555fce1
Vahag <135336961+Vahag5@users.noreply.github.com>
2023-06-20 10:36:24 +0400

    Update 10.06_7.py

--- 10.06_7.py
+++ 10.06_7.py
@@ -2,11 +2,12 @@ def convert_to_int(str1):
     if str1.isdigit():
         return int(str1)
     else:
-        raise Exception('Hnaravor chi')
+        raise Exception('Hnaravor chi poxakerpel')
 
-string2 = "123"
-print(convert_to_int(string2))
-
-
-string1 = "gntt"
-print(convert_to_int(string1))
+try:
+    string2 = '123'
+    print(convert_to_int(string2))
+    string1 = '33gntewft'
+    print(convert_to_int(string1))
+except Exception as s:
+    print('Exception occurred:', str(s))
